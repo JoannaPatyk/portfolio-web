b917742af1f57be20ba3c2049fbd69f6b4949e34
Joanna Patyk <patykjoanna93@gmail.com>
2023-01-08 18:45:22 +0100

    pierwsza wersja

---
+++ script.js
@@ -0,0 +1,12 @@
+$('.material-symbols-outlined').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.menu').offset().top
+    }, 500)
+})
+
+$('.menu a').on('click', function () {
+    const goToSection = "#" + $(this).attr('class');
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top
+    }, 500)
+})
